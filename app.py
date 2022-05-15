6e077423c53e9089ec68f71843fc558ffbc78e03
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2022-05-15 10:14:17 +0900

    ファイルアップロード

---
+++ app.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+import time
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# root アドレスのアクセス処理
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    print('on hello')
+    # flask 実行
+    app.run(host="127.0.0.1", port=5000)
---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+
+//button clicked
+function sample() {
+    document.getElementById('text').textContent = 'clicked!';
+}
