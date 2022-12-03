d07e47406f2c92756a17ae9c3ab143606bc0ba3b
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2022-12-03 12:25:42 +0900

    関連度の値を整数から少数に変更

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ class Relation():
         self.conn = sqlite3.connect("test.db")
     
 
-    def getRelevance(self, w1: str, w2: str) -> int:
+    def getRelevance(self, w1: str, w2: str) -> float:
         """
         DBから関連度を取得する
 
@@ -22,18 +22,18 @@ class Relation():
         cursor.close()
 
         ret = None
-        if res:  ret = res[2]
+        if res:  ret = float(res[2])
         return ret
         
 
-    def update(self, w1: str, w2: str, r: int) -> None:
+    def update(self, w1: str, w2: str, r: float) -> None:
         """
         単語の関連度を更新する。
 
         Args:
             w1 (str): 単語1
             w2 (str): 単語2
-            r (int): 設定する関連度
+            r (float): 設定する関連度
         """
         cursor = self.conn.cursor()
         h = self.__hashFormat(w1, w2)
@@ -52,14 +52,14 @@ class Relation():
         self.conn.commit()
     
 
-    def insert(self, w1: str, w2:str, r:int =0):
+    def insert(self, w1: str, w2:str, r:float =0) -> None:
         """
         単語同士の関連度をDBに追加する。
 
         Args:
             w1 (str): 単語1
             w2 (str): 単語2
-            r (int): 設定する関連度 (初期値=0)
+            r (float): 設定する関連度 (初期値=0)
         """
 
         cursor = self.conn.cursor()
@@ -83,16 +83,20 @@ class Relation():
         h2 = hashlib.sha256(w2.encode()).hexdigest()
         return (min(h1, h2), max(h1, h2))
 
-    def createDatabase(self):
+
+    def createDatabase(self) -> None:
         """
         DBテーブルを作成する。
         """
         cursor = self.conn.cursor()
         cursor.execute("create table Words(W1 nchar(64) , W2 nchar(64), R double, primary key(W1, W2));")
         self.conn.commit()
-    
+
+
+
 if __name__ == '__main__':
     a = Relation()
     # a.insert("こんにちは", "オレンジ")
-    print(a.getRelevance("こんにちは", "オレンジ"))
-    # a.update("こんにちは", "オレンジ", 5)
+    print(a.getRelevance("オレンジ", "こんにちは"))
+    a.update("こんにちは", "オレンジ", 5)
+    print(a.getRelevance("オレンジ", "こんにちは"))
